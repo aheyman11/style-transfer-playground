783939e52d579d982957cd2625a607460ce411b2
Andrea Heyman <aheyman11@gmail.com>
2016-09-29 14:33:59 -0400

    Updated number of iterations to user-specified input

--- app/views.py
+++ app/views.py
@@ -24,6 +24,7 @@ def upload_images():
 		filename = secure_filename(form.style_im.data.filename)
 		form.style_im.data.save('uploads/' + filename)
 		session['style_im'] = filename
+		session['num_iters'] = form.num_iter.data
 		return redirect(url_for('create_image'))
 	return render_template('upload_images.html', form=form)
 
@@ -46,15 +47,12 @@ def stream_template(template_name, **context):
 def create_image():
 	if 'style_im' in session:
 		style_im = session['style_im']
+		num_iters = session['num_iters']
 	else:
 		style_im = None
+		num_iters = 0
 	session.pop('style_im', None)
-	# def generate():
-	# 	for i in range(10):
-	# 		noise_image = np.random.uniform(0, 256,(224, 224, 3)).astype('float32')
-	# 		scipy.misc.imsave('out/' + str(i) + '.jpg', noise_image)
-	# 		time.sleep(1)
-	# 		yield(str(i) + '.jpg')
-	return Response(stream_template('create_image.html', style_im=style_im, data=make_image.run('./uploads/' + style_im, 1000)))
+	session.pop('num_iters', None)
+	return Response(stream_template('create_image.html', style_im=style_im, data=make_image.run('./uploads/' + style_im, num_iters)))
 
 app.secret_key = '\xbd\x90\xf9\x1e\xd4f/\xde\xef\xc2\x9b\x03\x9a/\x80\x15\xf6\x95\x0c\xf6\xf4\xb0\x10\x0e'
