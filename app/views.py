7ca0616cb6e0cc179ab5b4cbf3d57c55c69133ce
Andrea Heyman <aheyman11@gmail.com>
2016-09-29 11:08:36 -0400

    Implemented content streaming to display white noise images created on the fly

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
-from flask import render_template, flash, redirect, url_for, session, send_from_directory
+from flask import render_template, flash, redirect, url_for, session, send_from_directory, send_file, Response
 from werkzeug.utils import secure_filename
 from app import app
 from .forms import CreateImageForm
+
+import numpy as np
+import scipy.misc
+from PIL import Image
+import time
+from io import BytesIO
+import base64
 
 @app.route('/')
 @app.route('/index')
@@ -20,8 +27,18 @@ def upload_images():
 
 @app.route('/uploads/<filename>')
 def send_image(filename):
-	print("sending file..." + filename)
 	return send_from_directory('../uploads/', filename)
+
+@app.route('/out/<filename>')
+def send_out_image(filename):
+	print("sending out_image " + filename)
+	return send_from_directory('../out/', filename)
+
+def stream_template(template_name, **context):
+	app.update_template_context(context)
+	t = app.jinja_env.get_template(template_name)
+	rv = t.stream(context)
+	return rv
 
 @app.route('/create_image')
 def create_image():
@@ -31,5 +48,15 @@ def create_image():
 		style_im = None
 	session.pop('style_im', None)
 	return render_template('create_image.html', style_im=style_im)
+
+@app.route('/new_image')
+def generate_image():
+	def generate():
+		for i in range(10):
+			noise_image = np.random.uniform(0, 256,(224, 224, 3)).astype('float32')
+			scipy.misc.imsave('out/' + str(i) + '.jpg', noise_image)
+			time.sleep(1)
+			yield(str(i) + '.jpg')
+	return Response(stream_template('image.html', data=generate()))
 
 app.secret_key = '\xbd\x90\xf9\x1e\xd4f/\xde\xef\xc2\x9b\x03\x9a/\x80\x15\xf6\x95\x0c\xf6\xf4\xb0\x10\x0e'
