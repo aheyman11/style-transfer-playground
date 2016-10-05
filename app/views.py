8f033601e861bbf1529c7598671b521027260cc6
Andrea Heyman <aheyman11@gmail.com>
2016-10-05 16:17:03 -0400

    Added templates for 404 and 500 errors

--- app/views.py
+++ app/views.py
@@ -139,4 +139,13 @@ def delete_image():
 	print("file deleted")
 	return(jsonify({'success': True}))
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
 app.secret_key = '\xbd\x90\xf9\x1e\xd4f/\xde\xef\xc2\x9b\x03\x9a/\x80\x15\xf6\x95\x0c\xf6\xf4\xb0\x10\x0e'
